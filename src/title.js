7e791142b368e90292329e11d056a977c56a530e
Akinkoye Oluwafemi <akinfemi810@gmail.com>
2025-01-16 13:50:50 +0100

    Initialized a UI for LUMI

---
+++ src/title.js
@@ -0,0 +1,11 @@
+export const todayTitles = [
+      "Frontend Development Chat", 
+      "React Basics", 
+      "CSS Layout Help"
+]
+
+export const previousTitles =[
+      "JavaScript Functions", 
+      "HTML Structure", 
+      "CSS Flexbox Guide"
+]
